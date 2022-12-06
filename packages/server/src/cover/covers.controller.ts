cf8ad276e7cb1fe06a89011b047f977b58db3ede
Sergey Lenkov <serglazy@gmail.com>
2022-12-06 10:18:27 +0300

    WIP

--- packages/server/src/cover/covers.controller.ts
+++ packages/server/src/cover/covers.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get, Param, StreamableFile } from '@nestjs/common';
-import { createReadStream } from 'fs';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  StreamableFile,
+} from '@nestjs/common';
+import { createReadStream, existsSync } from 'fs';
 import { Cover } from './cover.entity';
 import { CoversService } from './covers.service';
 
@@ -19,9 +26,17 @@ export class CoversController {
 
   @Get(':id/file')
   async getFile(@Param('id') id): Promise<StreamableFile> {
-    const cover = await this.coversService.findById(id);
+    try {
+      const cover = await this.coversService.findById(id);
 
-    const file = createReadStream(cover.file);
-    return new StreamableFile(file);
+      if (existsSync(cover.file)) {
+        const file = createReadStream(cover.file);
+        return new StreamableFile(file);
+      } else {
+        throw new HttpException('File not exists', HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
