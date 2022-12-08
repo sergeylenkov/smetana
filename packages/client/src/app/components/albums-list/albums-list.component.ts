032ce7136a824167127567a058d6eebe3963f2cc
Sergey Lenkov <serglazy@gmail.com>
2022-12-08 09:54:24 +0300

    Save/restore scroll position

--- packages/client/src/app/components/albums-list/albums-list.component.ts
+++ packages/client/src/app/components/albums-list/albums-list.component.ts
@@ -1,6 +1,9 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { AlbumsService } from '../../services/albums.service';
 import { Album } from '../../dto/album';
+import { SettingsService } from 'src/app/services/settings.service';
+
+const DEFAULT_CARD_SIZE = 200;
 
 @Component({
   selector: 'app-albums-list',
@@ -10,10 +13,11 @@ import { Album } from '../../dto/album';
 })
 export class AlbumsListComponent implements OnInit, AfterViewInit {
   albums: Album[] = [];
-  cardSize: number = 250;
+  cardSize: number = DEFAULT_CARD_SIZE;
   @ViewChild('list') listElement?: ElementRef<HTMLDivElement>;
+  @ViewChild('scrollContainer') scrollContainer?: ElementRef<HTMLDivElement>;
 
-  constructor(private service: AlbumsService) { }
+  constructor(private service: AlbumsService, private settingsService: SettingsService) { }
 
   ngOnInit(): void {
     this.getAlbums();
@@ -22,9 +26,9 @@ export class AlbumsListComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     if (this.listElement) {
       const width = this.listElement.nativeElement.getBoundingClientRect().width;
-      let maxCount = Math.floor(width / 250);
+      let maxCount = Math.floor(width / DEFAULT_CARD_SIZE);
 
-      const freeSpace = width - maxCount * 250;
+      const freeSpace = width - maxCount * DEFAULT_CARD_SIZE;
 
       if (freeSpace < 100) {
         maxCount = maxCount - 1;
@@ -32,9 +36,19 @@ export class AlbumsListComponent implements OnInit, AfterViewInit {
 
       this.cardSize = Math.floor(width / maxCount);
     }
+
+    requestAnimationFrame(() => {
+      if (this.scrollContainer) {
+        this.scrollContainer.nativeElement.scrollTo(0, this.settingsService.scrollPosition);
+      }
+    });
   }
 
   async getAlbums() {
     this.albums = await this.service.getAlbums();
+  }
+
+  onScroll(e: Event): void {
+    this.settingsService.scrollPosition = (e.target as HTMLDivElement)?.scrollTop || 0;
   }
 }
--- packages/client/src/app/services/settings.service.ts
+++ packages/client/src/app/services/settings.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 
 enum StorageKeys {
-  Volume = 'volume'
+  Volume = 'Volume',
+  ScrollPosition = 'ScrollPosition'
 }
 
 @Injectable({
@@ -17,5 +18,13 @@ export class SettingsService {
 
   public get volume(): number {
     return Number(localStorage.getItem(StorageKeys.Volume)) || 0.5;
+  }
+
+  public set scrollPosition(value: number) {
+    localStorage.setItem(StorageKeys.ScrollPosition, String(value));
+  }
+
+  public get scrollPosition(): number {
+    return Number(localStorage.getItem(StorageKeys.ScrollPosition)) || 0;
   }
 }
