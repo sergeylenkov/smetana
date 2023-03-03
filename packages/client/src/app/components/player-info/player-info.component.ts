aa7b42686e4e04f5a466975671e9342b426b29ed
Sergey Lenkov <serglazy@gmail.com>
2023-03-03 16:19:58 +0300

    Show track, artist and album in title #8

--- packages/client/src/app/components/player-info/player-info.component.ts
+++ packages/client/src/app/components/player-info/player-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
+import { t } from '../../locales';
 import { Album } from '../../dto/album';
 import { Track } from '../../dto/track';
 import { PlayerService } from '../../services/player.service';
@@ -13,19 +15,27 @@ export class PlayerInfoComponent implements OnInit, OnDestroy {
   public track?: Track;
   public album?: Album;
   public artists: string = '';
-  private _subscription?: Subscription;
+  private _onStart?: Subscription;
+  private _onStop?: Subscription;
 
-  constructor(private playerService: PlayerService) {}
+  constructor(private playerService: PlayerService, private titleService: Title) {}
 
   ngOnInit(): void {
-    this._subscription = this.playerService.onStart.subscribe((track: Track) => {
+    this._onStart = this.playerService.onStart.subscribe((track: Track) => {
       this.track = this.playerService.track;
       this.album = this.playerService.album;
       this.artists = this.album?.artists.map(artist => artist.name).join(', ') || '';
+
+      this.titleService.setTitle(`${this.track?.title} - ${this.artists}`);
     });
+
+    this._onStop = this.playerService.onEnd.subscribe(() => {
+      this.titleService.setTitle(t('Smetana'));
+    })
   }
 
   ngOnDestroy(): void {
-    this._subscription && this._subscription.unsubscribe();
+    this._onStart && this._onStart.unsubscribe();
+    this._onStop && this._onStop.unsubscribe();
   }
 }
