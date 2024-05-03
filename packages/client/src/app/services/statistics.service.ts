e652e30d1d23aa4ea212c7c34373e6050b8f0315
Sergey Lenkov <serglazy@gmail.com>
2024-05-03 15:44:21 +0300

    Update client dependencies

--- packages/client/src/app/services/statistics.service.ts
+++ packages/client/src/app/services/statistics.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Album } from '../dto/album';
 import { Track } from '../dto/track';
 import { StatisticsAPI } from '../api/statistics.api';
-import { Artist } from '../dto/artist';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +9,7 @@ export class StatisticsService {
   constructor(private api: StatisticsAPI) {
   }
 
-  public async add(track: Track, album: Album, artist: Artist): Promise<void> {    
-    return this.api.add({ trackId: track.id, albumId: album.id, artistId: artist.id });
+  public async add(track: Track): Promise<void> {    
+    return this.api.add({ trackId: track.id });
   }
 }
